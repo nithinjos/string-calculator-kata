7da99aef39f2fca8ecc6eee774a7e99a1026f66f
Nithin Jose <nithinjose6.0@gmail.com>
2025-02-04 01:38:29 +0530

    feat: add function - handle single number

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,3 +1,5 @@
 def add(string):
     if string == "":
         return 0
+    else:
+        return int(string)
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -3,3 +3,11 @@ from src.string_calculator import add
 
 def test_add_empty_string():
     assert add("") == 0
+
+
+def test_add_single_number_1():
+    assert add("1") == 1
+
+
+def test_add_single_number_2():
+    assert add("2") == 2
