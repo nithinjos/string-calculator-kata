c4c53246ffcb848cfe7d748175218ac7d7cdf259
Nithin Jose <nithinjose6.0@gmail.com>
2025-02-04 02:39:41 +0530

    refactor: separate out get_numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,14 +1,18 @@
+def get_numbers(string):
+    if string.startswith("//"):
+        string = string.replace("//", "")
+        delimiter = string.split("\n")[0]
+        string = string.strip(delimiter).strip("\n")
+        string = string.replace(delimiter, ",")
+    string = string.replace("\n", ",")
+    return string.split(",")
+
+
 def add(string):
     if string == "":
         return 0
     else:
-        if string.startswith("//"):
-            string = string.replace("//", "")
-            delimiter = string.split("\n")[0]
-            string = string.strip(delimiter).strip("\n")
-            string = string.replace(delimiter, ",")
-        string = string.replace("\n", ",")
-        numbers = string.split(",")
+        numbers = get_numbers(string)
         negatives = [number for number in numbers if int(number) < 0]
         if negatives:
             raise ValueError(
